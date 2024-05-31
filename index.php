4dba371db118e7fb767561bce9f1ef9b9300bf82
destny69 <mzldestiny69@gmail.com>
2024-05-31 17:00:31 +0545

    first commit

--- index.php
+++ index.php
@@ -39,12 +39,32 @@
                         "INDIA" => "New Dehli",
                         "NEPAL" => "Kathmandu");
 
-    echo $capitals['NEPAL']; */
+    echo $capitals['NEPAL']; 
 
 
     foreach($_POST as $key => $value)
     {
         echo "{$key} = {$value}" ."<br>";
+    }
+    */
+    if(isset($_POST['login'])) 
+    {
+      
+
+        $username = $_POST["name"];
+        $password = $_POST["password"];
+
+        if(empty($username)){
+            echo "Please enter your username";
+        }
+     
+        elseif(empty($password)){
+            echo "Please enter your password";
+        }
+        else
+        {
+            echo "Welcome {$username}";
+        }
     }
 
 ?>
